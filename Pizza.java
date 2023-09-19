571e77a055ccae69e4349c8a1cf0e39b686a33f7
Darshan Github <dasrshan9008178816@gmail.com>
2023-09-19 21:07:10 +0530

    Modified the code to single class

--- Pizza.java
+++ Pizza.java
@@ -54,4 +54,15 @@ public class Pizza {
 		System.out.println("Total prize is : " + this.prize);
 		return this.prize;
 	}
+
+	public static void main(String[] args) {
+        Pizza p1 = new Pizza(false, "large", 1);
+        p1.addToppings(1);
+        p1.addCheese(1);
+        p1.parcel(1);
+        System.out.println("------------------------------------------------------------");
+        p1.getBill();
+    }
+
+}
 }
